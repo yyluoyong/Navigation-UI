1cbc35019a97f4ab0bae2ba84cc78450c9cb9f29
yyluoyong <zhenyanxl@qq.com>
2017-02-08 23:14:43 +0800

    实现通过点击Navigation的menu切换Fragment。

--- app/src/main/java/com/navigation_ui/MainActivity.java
+++ app/src/main/java/com/navigation_ui/MainActivity.java
@@ -3,6 +3,9 @@ package com.navigation_ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.navigation_ui.Fragment.FragmentButton;
+import com.navigation_ui.Fragment.FragmentIndex;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Fragment fragmentButton, fragmentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +47,18 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_camera); //初始化选择第一个按钮
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*
+        初始化第一个按钮对应的Fragment
+         */
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (fragmentButton == null) {
+            fragmentButton = new FragmentButton();
+            fragmentTransaction.add(R.id.content_main, fragmentButton);
+            fragmentTransaction.commit();
+        }
     }
 
     @Override
@@ -81,21 +100,79 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            handleNavCamera();
+        }
+        else if (id == R.id.nav_gallery) {
+            handleNavGallery();
+        }
+        else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_send) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * 隐藏已经初始化的Fragment
+     * @param tran
+     */
+    private void hideFragment(FragmentTransaction tran) {
+        if (fragmentButton != null) {
+            tran.hide(fragmentButton);
+        }
+        if (fragmentIndex != null) {
+            tran.hide(fragmentIndex);
+        }
+    }
+
+    /**
+     * 处理第一个按钮的事件
+     */
+    private void handleNavCamera() {
+
+        FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
+
+        hideFragment(tran);
+
+        if (fragmentButton == null) {
+            fragmentButton = new FragmentButton();
+            tran.add(R.id.content_main, fragmentButton);
+        }
+        else {
+            tran.show(fragmentButton);
+        }
+
+        tran.commit();
+    }
+
+    /**
+     * 处理第二个按钮的事件
+     */
+    private void handleNavGallery() {
+
+        FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
+
+        hideFragment(tran);
+
+        if (fragmentIndex == null) {
+            fragmentIndex = new FragmentIndex();
+            tran.add(R.id.content_main, fragmentIndex);
+        }
+        else {
+            tran.show(fragmentIndex);
+        }
+
+        tran.commit();
     }
 }
