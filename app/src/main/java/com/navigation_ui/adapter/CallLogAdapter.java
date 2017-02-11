50e19473ff6b643cef01594e8cfc23e2559f2d28
yyluoyong <zhenyanxl@qq.com>
2017-02-11 20:46:14 +0800

    添加主界面内容RecyclerView及其适配器，为RecyclerView的每项制作了基本布局（待改进）。

---
+++ app/src/main/java/com/navigation_ui/adapter/CallLogAdapter.java
@@ -0,0 +1,84 @@
+package com.navigation_ui.adapter;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.navigation_ui.R;
+import com.navigation_ui.model.CallLogItemModel;
+
+import java.util.List;
+
+/**
+ * Created by Yong on 2017/2/11.
+ */
+
+public class CallLogAdapter extends RecyclerView.Adapter<CallLogAdapter.ViewHolder> {
+
+    private List<CallLogItemModel> mCallLogList;
+
+    /**
+     * 内部类，对应布局文件 calllog_item.xml
+     */
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        View callLogItemView;
+
+        TextView contactsNameTV;
+        TextView phoneNumberTV;
+        TextView callDateTV;
+        TextView callCountsTV;
+        TextView durationTV;
+        TextView callTypeTV;
+        TextView callerLocTV;
+
+        public ViewHolder(View view) {
+            super(view);
+            callLogItemView = view;
+
+            contactsNameTV = (TextView) view.findViewById(R.id.contacts_name);
+            phoneNumberTV  = (TextView) view.findViewById(R.id.phone_number);
+            callDateTV     = (TextView) view.findViewById(R.id.call_date);
+            callCountsTV   = (TextView) view.findViewById(R.id.call_counts);
+            durationTV     = (TextView) view.findViewById(R.id.duration);
+            callTypeTV     = (TextView) view.findViewById(R.id.call_type);
+            callerLocTV    = (TextView) view.findViewById(R.id.caller_loc);
+        }
+    }
+
+    public CallLogAdapter(List<CallLogItemModel> callLogList) {
+        mCallLogList = callLogList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.calllog_item, parent, false);
+
+        ViewHolder holder = new ViewHolder(view);
+
+        //undo:为holder的组件添加监听事件
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        CallLogItemModel callLogItem = mCallLogList.get(position);
+
+        holder.contactsNameTV.setText(callLogItem.getContactsName());
+        holder.phoneNumberTV.setText(callLogItem.getPhoneNumber());
+        holder.callDateTV.setText(callLogItem.getDateInStr());
+        holder.callCountsTV.setText(callLogItem.getCallCounts());
+        holder.durationTV.setText(callLogItem.getDuration());
+        holder.callTypeTV.setText(callLogItem.getCallType());
+        holder.callerLocTV.setText(callLogItem.getCallerLoc());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCallLogList.size();
+    }
+}
---
+++ app/src/main/java/com/navigation_ui/model/CallLogItemModel.java
@@ -0,0 +1,95 @@
+package com.navigation_ui.model;
+
+
+/**
+ * Created by Yong on 2017/2/6.
+ */
+
+/**
+ * RecyclerView的适配类，对应布局文件 calllog_item.xml
+ */
+public class CallLogItemModel {
+
+    //姓名
+    private String contactsName;
+
+    //电话号码
+    private String phoneNumber;
+
+    //日期
+    private String dateInStr;
+
+    //通话次数
+    private int callCounts;
+
+    //通话时间，秒
+    private String duration;
+
+    //类型
+    private int callType;
+
+    //归属地
+    private String callerLoc;
+
+    public String getContactsName() {
+        return contactsName;
+    }
+
+    public void setContactsName(String contactsName) {
+        this.contactsName = contactsName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDateInStr() {
+        return dateInStr;
+    }
+
+    public void setDateInStr(String dateInStr) {
+        this.dateInStr = dateInStr;
+    }
+
+    public int getCallCounts() {
+        return callCounts;
+    }
+
+    public void setCallCounts(int callCounts) {
+        this.callCounts = callCounts;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public int getCallType() {
+        return callType;
+    }
+
+    public void setCallType(int callType) {
+        this.callType = callType;
+    }
+
+    public String getCallerLoc() {
+        return callerLoc;
+    }
+
+    public void setCallerLoc(String callerLoc) {
+        this.callerLoc = callerLoc;
+    }
+
+    @Override
+    public String toString() {
+        return  contactsName + " " + phoneNumber + " " + dateInStr +
+                " " + duration + " " + callType + " " + callerLoc + " " + callCounts;
+    }
+}
