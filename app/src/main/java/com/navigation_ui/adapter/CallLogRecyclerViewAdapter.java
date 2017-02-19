9d70674a3233d11a8ab5bdb485195f3f16e626c0
yyluoyong <yyluoyong@foxmail.com>
2017-02-19 22:03:57 +0800

    为组件添加监听，待完善监听事件处理方式。

--- app/src/main/java/com/navigation_ui/adapter/CallLogRecyclerViewAdapter.java
+++ app/src/main/java/com/navigation_ui/adapter/CallLogRecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.navigation_ui.adapter;
 
 
+import android.provider.CallLog;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 import com.navigation_ui.R;
 import com.navigation_ui.model.CallLogItemModel;
+import com.navigation_ui.tools.CallDateFormatter;
 import com.navigation_ui.tools.PhoneNumberFormatter;
 
 import java.util.List;
@@ -23,7 +26,7 @@ public class CallLogRecyclerViewAdapter extends RecyclerView.Adapter<CallLogRecy
     private List<CallLogItemModel> mCallLogList;
 
     /**
-     * 内部类，对应布局文件 calllog_item.xml
+     * 内部类，对应布局文件 calllog_item.xml，即通话列表每个单项
      */
     static class ViewHolder extends RecyclerView.ViewHolder {
         View callLogItemView;
@@ -32,8 +35,6 @@ public class CallLogRecyclerViewAdapter extends RecyclerView.Adapter<CallLogRecy
         TextView phoneNumberTV;   //电话号码
         TextView callDateTV;      //通话发生时间
         TextView callCountsTV;    //通话次数
-//        TextView durationTV;
-//        TextView callTypeTV;
         ImageView callTypeImage;  //通话类型对应的图片
         TextView callerLocTV;     //归属地
 
@@ -45,12 +46,8 @@ public class CallLogRecyclerViewAdapter extends RecyclerView.Adapter<CallLogRecy
             phoneNumberTV  = (TextView) view.findViewById(R.id.phone_number);
             callDateTV     = (TextView) view.findViewById(R.id.call_date);
             callCountsTV   = (TextView) view.findViewById(R.id.call_counts);
-//            durationTV     = (TextView) view.findViewById(R.id.duration);
-//            callTypeTV     = (TextView) view.findViewById(R.id.call_type);
             callTypeImage  = (ImageView) view.findViewById(R.id.call_type_image);
             callerLocTV    = (TextView) view.findViewById(R.id.caller_loc);
-
-
         }
     }
 
@@ -63,9 +60,29 @@ public class CallLogRecyclerViewAdapter extends RecyclerView.Adapter<CallLogRecy
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.calllog_item, parent, false);
 
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
 
-        //undo:为holder的组件添加监听事件
+        //undo:为单项上更多信息添加监听，计划是展示与该联系人历史通话详细信息。
+        holder.callLogItemView.findViewById(R.id.more_info).setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "点击了Info按钮，功能待完善.", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        );
+
+        //undo：为单项添加监听，计划是拨打条目的电话。
+        holder.callLogItemView.findViewById(R.id.calllog_detail_linearlayout).setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "拨打电话："+holder.phoneNumberTV.getText()
+                        .toString().replace(" ", ""),
+                        Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        );
 
         return holder;
     }
@@ -79,10 +96,19 @@ public class CallLogRecyclerViewAdapter extends RecyclerView.Adapter<CallLogRecy
         holder.phoneNumberTV.setText(PhoneNumberFormatter
             .phoneNumberFormat(callLogItem.getPhoneNumber()));
 
-        holder.callDateTV.setText(callLogItem.getDateInMilliseconds());
+        holder.callDateTV.setText(CallDateFormatter
+            .format(callLogItem.getDateInMilliseconds()));
+
         holder.callCountsTV.setText("("+callLogItem.getCallCounts()+")");
 
-        holder.callTypeImage.setImageResource(R.mipmap.ic_call_missed_grey600_18dp);
+        if (callLogItem.getCallType() == CallLog.Calls.INCOMING_TYPE) {
+            holder.callTypeImage.setImageResource(R.mipmap.ic_call_made_grey600_18dp);
+        } else if (callLogItem.getCallType() == CallLog.Calls.OUTGOING_TYPE) {
+            holder.callTypeImage.setImageResource(R.mipmap.ic_call_received_grey600_18dp);
+        } else {
+            holder.callTypeImage.setImageResource(R.mipmap.ic_call_missed_grey600_18dp);
+        }
+
         holder.callerLocTV.setText(callLogItem.getCallerLoc());
     }
 
