acb6d48647faf25733ef073b469f7bb66022415d
yyluoyong <yyluoyong@foxmail.com>
2017-02-27 23:31:25 +0800

    增了功能：读取通话记录并将其写入数据库。

--- app/src/main/java/com/navigation_ui/tools/MyApplication.java
+++ app/src/main/java/com/navigation_ui/tools/MyApplication.java
@@ -3,6 +3,9 @@ package com.navigation_ui.tools;
 import android.app.Application;
 import android.content.Context;
 
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 /**
  * Created by Yong on 2017/2/15.
  */
@@ -19,6 +22,11 @@ public class MyApplication extends Application {
         super.onCreate();
 
         context = getApplicationContext();
+
+        /**
+         * 对DBFlow进行初始化
+         */
+        FlowManager.init(new FlowConfig.Builder(this).build());
     }
 
     public static Context getContext() {
